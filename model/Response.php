19227c0ab15f061fd72d22afb51ecdbaf5bd93c9
K1Pro <support@k1pro.net>
2023-06-29 12:18:13 +0200

    PHP-Rest_API-Demo

---
+++ model/Response.php
@@ -0,0 +1,14 @@
+<?php
+
+    class Response {
+        
+        private $_success;
+        private $_httpStatusCode;
+        private $_messages = array();
+        private $_data;
+        private $_toCache = false;
+        private $_responseData = array();
+
+    }
+
+?>
