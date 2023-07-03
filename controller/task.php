2034d3963c95723ddc1fc8f0184a6591d6dc5689
K1Pro <support@k1pro.net>
2023-07-03 12:17:24 +0200

    created another endpoint for all tasks

--- controller/task.php
+++ controller/task.php
@@ -81,7 +81,7 @@
                 $response->send();
                 exit();
             }
-        } else if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+        } elseif ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
             try{
                 $query = $writeDB->prepare('delete from tbltasks where id = :taskid');
                 $query->bindParam(':taskid', $taskid, PDO::PARAM_INT);
@@ -113,7 +113,7 @@
                 $response->send();
                 exit;
             }
-        } else if ($_SERVER['REQUEST_METHOD'] === 'PATCH') {
+        } elseif ($_SERVER['REQUEST_METHOD'] === 'PATCH') {
 
         } else {
             $response = new Response();
@@ -123,6 +123,134 @@
             $response->send();
             exit;
         }
+    } elseif (array_key_exists('completed', $_GET)){
+        $completed = $_GET['completed'];
+
+        if($completed !== 'Y' && $completed !== 'N'){
+            $response = new Response();
+            $response->setHttpStatusCode(400);
+            $response->setSuccess(false);
+            $response->addMessage('Completed filter must be Y or N');
+            $response->send();
+            exit;
+        }
+
+        if($_SERVER['REQUEST_METHOD'] === 'GET') {
+            try{
+                $query = $readDB->prepare('select id, title, description, DATE_FORMAT(deadline, "%d/%m/%Y %H:%i") as deadline, completed from tbltasks where completed = :completed');
+                $query->bindParam(':completed', $completed, PDO::PARAM_STR);
+                $query->execute();
+
+                $rowCount = $query->rowCount();
+
+                $taskArray = array();
+
+                while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+                    $task = new Task($row['id'], $row['title'], $row['description'], $row['deadline'], $row['completed']);
+                    $taskArray[] = $task->returnTaskAsArray();
+                }
+
+                $returnData = array();
+                $returnData['rows_returned'] = $rowCount;
+                $returnData['tasks'] = $taskArray;
+
+                $response = new Response;
+                $response->setHttpStatusCode(200);
+                $response->setSuccess(true);
+                $response->toCache(true);
+                $response->setData($returnData);
+                $response->send();
+                exit;
+            }
+            catch(TaskException $ex) {
+                $response = new Response();
+                $response->setHttpStatusCode(500);
+                $response->setSuccess(false);
+                $response->addMessage($ex->getMessage());
+                $response->send();
+                exit;
+            }
+            catch (PDOException $ex){
+                error_log('Database query error -'.$ex, 0);
+                $response = new Response();
+                $response->setHttpStatusCode(500);
+                $response->setSuccess(false);
+                $response->addMessage('Failed to get tasks');
+                $response->send();
+                exit;
+            }
+        } else {
+            $response = new Response();
+            $response->setHttpStatusCode(405);
+            $response->setSuccess(false);
+            $response->addMessage('Request method not allowed');
+            $response->send();
+            exit;
+        }
+    } elseif (empty($_GET)) {
+        if($_SERVER['REQUEST_METHOD'] === 'GET') {
+            try{
+                $query = $readDB->prepare('select id, title, description, DATE_FORMAT(deadline, "%d/%m/%Y %H:%i") as deadline, completed from tbltasks');
+                $query->execute();
+
+                $rowCount = $query->rowCount();
+
+                $taskArray = array();
+
+                while($row = $query->fetch(PDO::FETCH_ASSOC)) {
+                    $task = new Task($row['id'], $row['title'], $row['description'], $row['deadline'], $row['completed']);
+                    $taskArray[] = $task->returnTaskAsArray();
+                }
+
+                $returnData = array();
+                $returnData['rows_returned'] = $rowCount;
+                $returnData['tasks'] = $taskArray;
+
+                $response = new Response();
+                $response->setHttpStatusCode(200);
+                $response->setSuccess(true);
+                $response->toCache(true);
+                $response->setData($returnData);
+                $response->send();
+                exit;
+
+            }
+            catch(TaskException $ex){
+                
+                $response = new Response();
+                $response->setHttpStatusCode(500);
+                $response->setSuccess(false);
+                $response->addMessage($ex->getMessage());
+                $response->send();
+                exit;
+            }
+            catch(PDOException $ex){
+                error_log('Database query error - '.$ex, 0);
+                $response = new Response();
+                $response->setHttpStatusCode(500);
+                $response->setSuccess(false);
+                $response->addMessage('Failed to get tasks');
+                $response->send();
+                exit;
+            }
+
+        } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        } else {
+            $response = new Response();
+            $response->setHttpStatusCode(405);
+            $response->setSuccess(false);
+            $response->addMessage('Request method not allowed');
+            $response->send();
+            exit;
+        }
+    } else {
+        $response = new Response();
+        $response->setHttpStatusCode(404);
+        $response->setSuccess(false);
+        $response->addMessage('Endpoint not found');
+        $response->send();
+        exit;
     }
 
 
