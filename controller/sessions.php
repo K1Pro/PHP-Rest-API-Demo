0e2914d6c342b8a5b36ad866a703ba0957d949de
K1Pro <support@k1pro.net>
2023-07-13 14:56:57 +0200

    creating access and refresh tokens

--- controller/sessions.php
+++ controller/sessions.php
@@ -31,7 +31,128 @@ else if(empty($_GET)){
     
     sleep(1); // delays the whole processing by 1 second so hackers can't do brute force dictionary attack
 
+    if($_SERVER['CONTENT_TYPE'] !== 'application/json') {
+        $response = new Response();
+        $response->setHttpStatusCode(400);
+        $response->setSuccess(false);
+        $response->addMessage('Content Type header not set to JSON');
+        $response->send();
+        exit;
+    }
+
+    $rawPostData = file_get_contents('php://input');
     
+    if(!$jsonData = json_decode($rawPostData)) {
+        $response = new Response();
+        $response->setHttpStatusCode(400);
+        $response->setSuccess(false);
+        $response->addMessage('Request body is not valid JSON');
+        $response->send();
+        exit;
+    }
+
+    if(!isset($jsonData->username) || !isset($jsonData->password)) {
+        $response = new Response();
+        $response->setHttpStatusCode(400);
+        $response->setSuccess(false);
+        (!isset($jsonData->username) ? $response->addMessage('Username not supplied') : false);
+        (!isset($jsonData->password) ? $response->addMessage('Password not supplied') : false);
+        $response->send();
+        exit;
+    }
+
+    // you can insert more password checks here, such as one uppercase, lowercase and number....
+    if(strlen($jsonData->username) < 1 || strlen($jsonData->username) > 255 || strlen($jsonData->password) < 1 || strlen($jsonData->password) > 255){
+        $response = new Response();
+        $response->setHttpStatusCode(400);
+        $response->setSuccess(false);
+        (strlen($jsonData->username) < 1 ? $response->addMessage('Username cannot be blank') : false);
+        (strlen($jsonData->username) > 255 ? $response->addMessage('Username cannot be greater than 255 characters') : false);
+        (strlen($jsonData->password) < 1 ? $response->addMessage('Password cannot be blank') : false);
+        (strlen($jsonData->password) > 255 ? $response->addMessage('Password cannot be greater than 255 characters') : false);
+        $response->send();
+        exit;
+    }
+
+    try {
+        $username = $jsonData->username;
+        $password = $jsonData->password;
+
+        $query = $writeDB->prepare('select id, fullname, username, password, useractive, loginattempts from tblusers where username = :username');
+        $query->bindParam(':username', $username, PDO::PARAM_STR);
+        $query->execute();
+
+        $rowCount = $query->rowCount();
+        
+        if($rowCount === 0) {
+            $response = new Response();
+            $response->setHttpStatusCode(401);
+            $response->setSuccess(false);
+            $response->addMessage('Username or password is incorrect');
+            $response->send();
+            exit;
+        }
+
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+
+        $returned_id = $row['id'];
+        $returned_fullname = $row['fullname'];
+        $returned_username = $row['username'];
+        $returned_passowrd = $row['password'];
+        $returned_useractive = $row['useractive'];
+        $returned_loginattempts = $row['loginattempts'];
+
+        if($returned_useractive !== 'Y') {
+            $response = new Response();
+            $response->setHttpStatusCode(401);
+            $response->setSuccess(false);
+            $response->addMessage('User account not active');
+            $response->send();
+            exit;
+        }
+
+        if($returned_loginattempts >= 3) {
+            $response = new Response();
+            $response->setHttpStatusCode(401);
+            $response->setSuccess(false);
+            $response->addMessage('User account is currently locked out');
+            $response->send();
+            exit;
+        }
+
+        if(!password_verify($password, $returned_passowrd)) {
+            $query = $writeDB->prepare('update tblusers set loginattempts = loginattempts+1 where id = :id ');
+            $query->bindParam(':id', $returned_id, PDO::PARAM_INT);
+            $query->execute();
+
+            $response = new Response();
+            $response->setHttpStatusCode(401);
+            $response->setSuccess(false);
+            $response->addMessage('Username or password is incorrect');
+            $response->send();
+            exit;
+        }
+
+        // finished at 52:00 on 026POST video
+
+        $accesstoken = base64_encode(bin2hex(openssl_random_pseudo_bytes(24)).time());
+        $refreshtoken = base64_encode(bin2hex(openssl_random_pseudo_bytes(24)).time());
+
+        $access_token_expiry_seconds = 1200;
+        $refresh_token_expiry_seconds = 1209600;
+
+
+
+    }
+    catch(PDOException $ex){
+        $response = new Response();
+        $response->setHttpStatusCode(500);
+        $response->setSuccess(false);
+        $response->addMessage('There was an issue logging in');
+        $response->send();
+        exit;
+    }
+
 }
 else {
     $response = new Response();
